4c66e0876bc90ef9dfcd045a66158216c781b4e2
MORAN7456 <152824728+MORAN7456@users.noreply.github.com>
2023-12-04 05:09:07 +0400

    2

---
+++ 24(1)/24/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _24
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string login = textBox1.Text;
+            string password = textBox2.Text;
+
+            if (login == "Админ" && password == "Админ")
+            {
+                // Открываем другую форму
+                Form1 form1 = new Form1();
+                form1.Show();
+
+                // Закрываем текущую форму
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ 24(1)/24/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _24
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form2());
+        }
+    }
+    static class Database
+    {
+        public static string connection = @"Data Source=24.db; Integrated Security=False; MultipleActiveResultSets=True";
+    }
+    static class qtable
+    {
+        public static string main = "Process";
+        public static string ID = "Id";
+        public static string NP = "NameProcess";
+        public static string Pr = "Priority";
+        public static string Class = "Class";
+        public static string OI = "OwnerID";
+    }
+    static class wtable
+    {
+        public static string main = "Recourcces";
+        public static string ID = "Id";
+        public static string RN = "ResoirceName";
+        public static string Kol = "Kol-voR";
+        public static string Price = "Price";
+    }
+    static class etable
+    {
+        public static string main = "PlannedResources";
+        public static string ID = "Id";
+        public static string ProcessID = "ProcessID";
+        public static string Priority = "Priority";
+        public static string ResourceID = "ResourceID";
+        public static string Kolvo = "Kolvo";
+        public static string Requested = "Requested";
+        public static string Highlighted = "Highlighted";
+        public static string Owner = "Owner";
+        public static string Price = "Price";
+    }
+}
